6582c87cfbe720d6d08909a146872ad2b844497d
oleksandrhavryush <havryush@gmail.com>
2024-05-06 11:45:19 +0200

    Refactored tests to use H2 in-memory database
    - Defined two beans for H2 and PostgreSQL databases.
    - Configured application to select the appropriate bean based on the active Spring profile.
    - Simplified test data scripts to be compatible with H2.

---
+++ src/main/java/com/javarush/jira/common/internal/config/DatabaseConfig.java
@@ -0,0 +1,44 @@
+package com.javarush.jira.common.internal.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfig {
+
+    private String dbUrl = "jdbc:postgresql://localhost:5433/jira-test";
+
+    @Value("${TEST_DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${TEST_DB_PASSWORD}")
+    private String dbPassword;
+
+    @Bean
+    @Profile("testPostgres")
+    public DataSource postgresDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    @Bean
+    @Profile("testH2")
+    public DataSource h2DataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+}
--- src/test/java/com/javarush/jira/login/internal/web/UserControllerTest.java
+++ src/test/java/com/javarush/jira/login/internal/web/UserControllerTest.java
@@ -58,7 +58,7 @@ class UserControllerTest extends AbstractControllerTest {
 
     @Test
     void createWithLocation() throws Exception {
-        UserTo newTo = mapper.toTo(getNew());
+        UserTo newTo = mapper.toTo(getNew2());
         ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(jsonWithPassword(newTo, newTo.getPassword())))
@@ -66,7 +66,7 @@ class UserControllerTest extends AbstractControllerTest {
 
         User created = USER_MATCHER.readFromJson(action);
         long newId = created.id();
-        User newUser = getNew();
+        User newUser = getNew2();
         newUser.setId(newId);
         USER_MATCHER.assertMatch(created, newUser);
         USER_MATCHER.assertMatch(repository.getExisted(newId), newUser);
